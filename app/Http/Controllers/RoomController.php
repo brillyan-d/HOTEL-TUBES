6fdf5289964699ce9a1504b200df1f564b33f064
brillyan-d <eviljakeeen09857@gmail.com>
2025-12-09 14:00:56 +0700

    Rename KamarController to RoomController

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
 
-class KamarController extends Controller
+class RoomController extends Controller
 {
     // =========================================================
     // Admin CRUD Methods

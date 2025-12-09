97646605e854080652dfa626bd0a548c1a98dda8
brillyan-d <eviljakeeen09857@gmail.com>
2025-12-09 14:06:16 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,20 +8,33 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
+        // 1. Akun Admin (role: admin)
         User::create([
-            'name' => 'AdminHotel',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('admin123'),
-            'role' => 'admin'
+            'name' => 'Hotel Admin',
+            'email' => 'admin@hotel.com', // Email Login Admin
+            'password' => Hash::make('password'), // Password Default: password
+            'role' => 'admin',
         ]);
 
+        // 2. Akun User Biasa (role: user)
         User::create([
-            'name' => 'User Biasa',
-            'email' => 'user@example.com',
-            'password' => Hash::make('user123'),
-            'role' => 'user'
+            'name' => 'Customer Test',
+            'email' => 'user@hotel.com', // Email Login User
+            'password' => Hash::make('password'), // Password Default: password
+            'role' => 'user',
+        ]);
+
+        // Default User
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'user', // Tambahkan role default
         ]);
     }
 }
